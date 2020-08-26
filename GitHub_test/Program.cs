5234a23cdbff45fb9f330a93468bd890fe76c076
William <william.marback@hotmail.com>
2020-08-26 10:57:08 +0200

    Initial commit

---
+++ GitHub_test/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitHub_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hej Micke!");
+            Console.ReadLine();
+        }
+    }
+}
